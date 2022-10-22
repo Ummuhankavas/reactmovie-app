b47980ee2bfe4888723e5a06f4f9815e65ca453f
Ummuhankavas <ummuhankavas@gmail.com>
2022-10-22 14:34:26 +0200

    navbarbrand done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <Link to={'/'}  className="navbar-brand text-white">
+          <h4>React Movie App</h4>
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
