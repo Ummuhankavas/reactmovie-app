b7690103828a6730bbacce38f995e1829177b554
Ummuhankavas <ummuhankavas@gmail.com>
2022-10-24 20:55:52 +0200

    movie details created

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
 const IMG_API = 'https://image.tmdb.org/t/p/w1280';
@@ -7,7 +8,8 @@ const defaultImage =
 
 const MovieCard = ({poster_path,title,overview,vote_average, id}) => {
 
-    const {currentUser} = useContext(AuthContext)
+    const {currentUser} = useContext(AuthContext);
+    const navigate = useNavigate();
     const setVoteClass = (vote) => {
         if (vote>8){
             return 'green'
@@ -18,7 +20,7 @@ const MovieCard = ({poster_path,title,overview,vote_average, id}) => {
         }
     };
   return (
-    <div className='movie'>
+    <div className='movie' onClick={()=> navigate('/details/'+id)}>
         <img 
         loading= 'lazy'
         src= {poster_path ? IMG_API + poster_path : defaultImage} 
